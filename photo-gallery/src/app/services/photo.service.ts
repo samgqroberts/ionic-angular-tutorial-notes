f3cc4ae5a911f13a586cb606d3947d8d2eb897f4
Sam Roberts <samgqroberts@gmail.com>
2021-08-09 17:48:37 -0400

    tutorial page: Saving Photos to the Filesystem

--- photo-gallery/src/app/services/photo.service.ts
+++ photo-gallery/src/app/services/photo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource, Photo as CameraPhoto } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 
@@ -18,11 +18,48 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100,
     });
+    const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift({
       filepath: 'soon...',
       webviewPath: capturedPhoto.webPath,
     });
   }
+
+  public async savePicture(cameraPhoto: CameraPhoto): Promise<Photo> {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath,
+    };
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath);
+    const blob = await response.blob();
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
 
 export interface Photo {
