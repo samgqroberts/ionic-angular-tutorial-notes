bbcd7127f2bbbfedd52777a5a2ac53d39647b4b6
Sam Roberts <samgqroberts@gmail.com>
2021-08-09 17:37:44 -0400

    tutorial page: Taking Photos with the Camera

---
+++ photo-gallery/src/app/services/photo.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public photos: Photo[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+    this.photos.unshift({
+      filepath: 'soon...',
+      webviewPath: capturedPhoto.webPath,
+    });
+  }
+}
+
+export interface Photo {
+  filepath: string;
+  webviewPath: string;
+}
